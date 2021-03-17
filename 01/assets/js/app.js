9208338522232c54d39c6b12b89d54e5dc90744d
3Bartnik <ebartnik@o2.pl>
2021-03-17 15:58:53 +0100

    Maked arithmetic operation on variables

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -5,3 +5,12 @@ console.log(a, b);
 
 console.log(typeof(a));
 console.log(typeof(b));
+
+const numA= Number(a);
+
+const sum= numA * b;
+const subtr = numA - b;
+const multi = numA * b;
+const divide = numA / b;
+const expo = numA ** b;
+console.log( sum, subtr, multi, divide, expo);
