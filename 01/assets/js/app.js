3a35aecaf48216a6508e61f16c2ea5b5b414ad5f
3Bartnik <ebartnik@o2.pl>
2021-03-17 16:17:49 +0100

    Sorted scores into greather & less than 20

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -13,4 +13,26 @@ const subtr = numA - b;
 const multi = numA * b;
 const divide = numA / b;
 const expo = numA ** b;
-console.log( sum, subtr, multi, divide, expo);
+console.log(sum, subtr, multi, divide, expo);
+
+let myArray= [sum, subtr, multi, divide, expo];
+let greaterThan = '';
+let lessThan = '';
+for (let i = 0; i < myArray.length; i++) {
+    let result = myArray[i];
+    if (result > 20) {
+        if (greaterThan === '') {
+            greaterThan += result;
+        } else {
+               greaterThan += ', ' + result;
+        }
+    } else {
+        if (lessThan === '') {
+            lessThan += result;
+        } else {
+                lessThan += ', ' + result;
+        }
+    }
+}
+console.log(`Liczby większe od 20 to : ${greaterThan}`);
+console.log(`Liczby mniejsze od 20 to :' ${lessThan}`);
