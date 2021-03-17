d25b4f45d182f20526c5c2b0053406390d508eaa
3Bartnik <ebartnik@o2.pl>
2021-03-18 00:09:07 +0100

    Fixed for loop. Multiplication with selected number

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,14 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
-for (let i= 1; i <= 9; i++) {
-    let multiply = x * i;
-     console.log(`${i} x ${x} = ${multiply}`);
+const x = prompt('Podaj liczbę!');
+if (x <= 9 && x > 1) {
+    for (let i= 1; i <= 9; i++) {
+        let multiply = x * i;
+        console.log(`${i} x ${x} = ${multiply}`);
+    }
+} else {
+    alert ("Proszę wybrać liczbę od 1 do 9");
 }
-
 
 
 /* rozwiązanie z pętlą while  */
